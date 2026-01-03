3c86b1f5e69ef8acfeb468d779a4bc2077a812f4
Casey Manion <casey@manion.com>
2026-01-02 19:56:34 -0800

    new tabs for links

--- new_homes.js
+++ new_homes.js
@@ -132,7 +132,7 @@ console.log(mdDivider);
 newHomes.filter(h => !h.underContract).forEach(h => {
   console.log([
     "",
-    `<a href="${h.link}" target="_blank" rel="noopener noreferrer">${h.property}</a>`,
+    `<a href="${h.link}" class="targetBlank" target="_blank" rel="noopener noreferrer">${h.property}</a>`,
     h.priceString,
     formatToDollars(h.montlyEst, false),
     formatToDollars(h.pricePerSqFt, false),
